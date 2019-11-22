4f4732ab5771eac7a45e295aa44df434df047adb
Steven Bull <stevenwbulljr@gmail.com>
2019-11-22 18:26:05 +0000

    insert initial html/xml/jsx

--- src/App.js
+++ src/App.js
@@ -3,10 +3,64 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <>
+      <nav>
+        <em>&lt;  STEVEN W. BULL JR. &gt;</em>
+      </nav>
+      <main>
+          <header>
+              <p>Hello, my name is</p>
+              <h1>Steven</h1>
+              <p>and I'm a chemist turned</p>
+              <h2>Full-Stack Engineer</h2>
+          </header>
+
+          <section>
+              <h3>About Me</h3>
+              <div class="aboutMe">
+                <p>
+                  I am a current apprentice for Thinkful in the Engineering Immersion program.
+                  I love the smell of strong coffee in front of a computer monitor in the morning,
+                  and I love solving problems and squashing bugs even more.
+                  The endorphin rush I experience whenever I finally publish a project can rival any adrenaline junkie’s high.
+                  I enjoy learning new things and breaking everything while putting those new things into practice. 
+                </p>
+                <p>
+                  When I’m not in front of a computer monitor, I can be found spending time 
+                  with my wife and three (large) dogs or binge-watching various types of 
+                  documentaries on Netflix. 
+                  My current one-year learning goals (beyond coding) are the piano and the 
+                  Spanish language. Maybe if Spanish were a computer language, 
+                  I could learn it quicker than I currently am…
+                </p>
+              </div>
+          </section>
+
+          <section class="skills">
+              <h3>My Skills</h3>
+              <div>
+                <p>HTML | CSS | Javascript | jQuery | Python</p>
+              </div>
+          </section>
+
+          <section class="projects">
+              <h3>Projects</h3>
+              <div>
+                <img src="chemistryKnowledgeQuiz.png" alt="Chemistry Knowledge Quiz App" />
+                <h4>The Chemistry Knowledge Quiz </h4>
+                <p><a href="https://github.com/thinkful-ei-gecko/smiti-steven-chemistquizapp">Repo</a> | <a href="https://thinkful-ei-gecko.github.io/smiti-steven-chemistquizapp/">Live</a></p>
+                <p><em>Engineered with HTML, CSS, & JavaScript</em></p>
+                <p>The Chemistry Knowledge Quiz application will test your comprehension of the chemistry field! (Or how much you remember from high-school / college). </p>
+              </div>
+          </section>
+
+      </main>
+      <footer><a 
+        href="mailto:stevenwbulljr@gmail.com" 
+        target="_blank" rel="noopener noreferrer">Email</a> | <a href="https://github.com/StevenWBull" 
+        target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="https://www.linkedin.com/in/stevenwbull/" 
+        target="_blank" rel="noopener noreferrer">LinkedIn</a></footer>
+    </>
   );
 }
 
