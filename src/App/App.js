f2c4a0f31eb71d0fa91f1dae6859d0ade6988fff
Steven Bull <stevenwbulljr@gmail.com>
2019-12-02 22:17:52 +0000

    encrypt email to hide from web crawlers

--- src/App/App.js
+++ src/App/App.js
@@ -56,7 +56,7 @@ function App() {
 
       </main>
       <footer><a 
-        href="mailto:stevenwbulljr@gmail.com" 
+        href="mailto:mailto:&#115;&#116;&#101;&#118;&#101;&#110;&#119;&#098;&#117;&#108;&#108;&#106;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;" 
         target="_blank" rel="noopener noreferrer">Email</a> | <a href="https://github.com/StevenWBull" 
         target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="https://www.linkedin.com/in/stevenwbull/" 
         target="_blank" rel="noopener noreferrer">LinkedIn</a>
