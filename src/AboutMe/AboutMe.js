2ac5e01053e5b902f88c2bc60a761c71530e31cc
Steven Bull <stevenwbulljr@gmail.com>
2019-12-02 23:37:41 +0000

    slight syntax tweaks

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -3,24 +3,24 @@ import React from 'react';
 export default function AboutMe() {
   return (
     <section>
-              <h3>About Me</h3>
-              <div className="aboutMe">
-                <p>
-                  I am a current apprentice for Thinkful in the Engineering Immersion program.
-                  I love the smell of strong coffee in front of a computer monitor in the morning,
-                  and I love solving problems and squashing bugs even more.
-                  The endorphin rush I experience whenever I finally publish a project can rival any adrenaline junkie’s high.
-                  I enjoy learning new things and breaking everything while putting those new things into practice. 
-                </p>
-                <p>
-                  When I’m not in front of a computer monitor, I can be found spending time 
-                  with my wife and three (large) dogs or binge-watching various types of 
-                  documentaries on Netflix. 
-                  My current one-year learning goals (beyond coding) are the piano and the 
-                  Spanish language. Maybe if Spanish were a computer language, 
-                  I could learn it quicker than I currently am…
-                </p>
-              </div>
-          </section>
+      <h3>About Me</h3>
+      <div className="aboutMe">
+        <p>
+          I am a current apprentice for Thinkful in the Engineering Immersion program.
+          I love the smell of strong coffee in front of a computer monitor in the morning,
+          and I love solving problems and squashing bugs even more.
+          The endorphin rush I experience whenever I finally publish a project can rival any adrenaline junkie’s high.
+          I enjoy learning new things and breaking everything while putting those new things into practice. 
+        </p>
+        <p>
+          When I’m not in front of a computer monitor, I can be found spending time 
+          with my wife and three (large) dogs or binge-watching various types of 
+          documentaries on Netflix. 
+          My current one-year learning goals (beyond coding) are the piano and the 
+          Spanish language. Maybe if Spanish were a computer language, 
+          I could learn it quicker than I currently am…
+        </p>
+      </div>
+    </section>
   )
 }
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer><a 
-        href="mailto:mailto:&#115;&#116;&#101;&#118;&#101;&#110;&#119;&#098;&#117;&#108;&#108;&#106;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;" 
-        target="_blank" rel="noopener noreferrer">Email</a> | <a href="https://github.com/StevenWBull" 
-        target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="https://www.linkedin.com/in/stevenwbull/" 
-        target="_blank" rel="noopener noreferrer">LinkedIn</a>
-      </footer>
+    <footer>
+      <a 
+      href="mailto:mailto:&#115;&#116;&#101;&#118;&#101;&#110;&#119;&#098;&#117;&#108;&#108;&#106;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;" 
+      target="_blank" 
+      rel="noopener noreferrer">Email</a> | 
+      <a 
+      href="https://github.com/StevenWBull" 
+      target="_blank" 
+      rel="noopener noreferrer">GitHub</a> | 
+      <a 
+      href="https://www.linkedin.com/in/stevenwbull/" 
+      target="_blank" 
+      rel="noopener noreferrer">LinkedIn</a>
+    </footer>
   )
 }
