f9e7be581e39a5708d81af1b1b4e4f6d0d7f0f6e
Steven Bull <stevenwbulljr@gmail.com>
2021-12-09 17:15:25 -0600

    checking a hunch

--- stevenb-portfolio-client/src/AnaBabyShowerWebsite/Nav/Nav.js
+++ stevenb-portfolio-client/src/AnaBabyShowerWebsite/Nav/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Nav() {
   return(
     <nav>
-      <em>&lt;  ANA & STEVEN BUL &#47;&gt;</em>
+      <em>&lt;  ANA & STEVEN BULL &#47;&gt;</em>
     </nav>
   )
 }
