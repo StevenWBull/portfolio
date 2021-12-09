0edb1508904e289abb68281901f6e403dc26668c
Steven Bull <stevenwbulljr@gmail.com>
2021-12-09 17:10:43 -0600

    compartmentalize different App interfaces

--- stevenb-portfolio-client/src/AnaBabyShowerWebsite/Footer/Footer.js
+++ stevenb-portfolio-client/src/AnaBabyShowerWebsite/Footer/Footer.js
@@ -1,30 +1,6 @@
 import React from 'react';
 import './Footer.css';
 
-// export default function Footer() {
-//   return (
-//     <footer className='footerContainer'>
-//       <a href='mailto:&#115;&#116;&#101;&#118;&#101;&#110;&#119;&#098;&#117;&#108;&#108;&#106;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;'
-//         target='_blank'
-//         rel="noopener noreferrer">
-//           <ion-icon name="mail"></ion-icon>
-//       </a>
-
-//       <a href='https://www.linkedin.com/in/stevenwbull/'
-//         target='_blank'
-//         rel="noopener noreferrer">
-//           <ion-icon name="logo-linkedin"></ion-icon>
-//       </a>
-
-//       <a href='https://github.com/StevenWBull'
-//         target='_blank'
-//         rel="noopener noreferrer">
-//           <ion-icon name="logo-github"></ion-icon>
-//       </a>
-//     </footer>
-//   )
-// }
-
 export default function Footer() {
   return (
     <footer className='footerContainer'>
