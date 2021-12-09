5ba46b4b65bba9357170d5b5d73c4ace4d1e7e4e
Steven Bull <stevenwbulljr@gmail.com>
2021-12-09 17:10:15 -0600

    changes

--- stevenb-portfolio-client/src/AnaBabyShowerWebsite/Footer/Footer.js
+++ stevenb-portfolio-client/src/AnaBabyShowerWebsite/Footer/Footer.js
@@ -28,7 +28,11 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer className='footerContainer'>
-      Designed, Built, & Powered by Steven Bull.
+      <a href='mailto:&#115;&#116;&#101;&#118;&#101;&#110;&#119;&#098;&#117;&#108;&#108;&#106;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;'
+        target='_blank'
+        rel="noopener noreferrer">
+          Designed, Built, & Powered by Steven Bull.
+      </a>
     </footer>
   )
 }
--- stevenb-portfolio-client/src/PortfolioWebsite/App/App.js
+++ stevenb-portfolio-client/src/PortfolioWebsite/App/App.js
@@ -6,7 +6,6 @@ import AboutMe from '../AboutMe/AboutMe';
 import Skills from '../Skills/Skills';
 import Projects from '../Projects/Projects';
 import Accordion from '../Accordion/Accordion';
-import MaintenancePage from '../../AnaBabyShowerWebsite/MaintenancePage/MaintenancePage';
 import './App.css';
 
 function App() {
@@ -14,11 +13,10 @@ function App() {
     <>
     <Nav />
       <main>
-        <MaintenancePage />
-        {/* <Header />
+        <Header />
         <Accordion title='About Me' component={AboutMe} />
         <Accordion title='Skills' component={Skills} />
-        <Accordion title='Projects' component={Projects} /> */}
+        <Accordion title='Projects' component={Projects} />
         <Footer />
       </main>
     </>
