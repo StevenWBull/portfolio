5447e55acd97f82048c925ecfc99532aa9f71e3f
Steven Bull <stevenwbulljr@gmail.com>
2021-12-09 17:12:56 -0600

    compartmentalize different App interfaces

--- stevenb-portfolio-client/src/AnaBabyShowerWebsite/Nav/Nav.js
+++ stevenb-portfolio-client/src/AnaBabyShowerWebsite/Nav/Nav.js
@@ -1,13 +1,5 @@
 import React from 'react';
 
-// export default function Nav() {
-//   return(
-//     <nav>
-//       <em>&lt;  STEVEN W. BULL JR. &#47;&gt;</em>
-//     </nav>
-//   )
-// }
-
 export default function Nav() {
   return(
     <nav>
